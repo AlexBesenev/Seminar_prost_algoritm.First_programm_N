1f285dff97e729ab6c56fcb077c685e69a3718d6
Alex Besenev <Besenev96@gmail.com>
2024-02-10 19:43:32 +0500

    add 1task

---
+++ 1task_3_number/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая принимает на вход
+трёхзначное число и удаляет вторую цифру этого
+числа.
+Примеры
+a = 256 => 26
+a = 891 => 81
+*/
+
+int a = 891;
+int first_number = a / 100;
+double second_number = a % 10;
+System.Console.Write($"{first_number}" + $"{second_number}");
