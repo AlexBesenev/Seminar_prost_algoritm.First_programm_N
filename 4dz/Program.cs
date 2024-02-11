b0401eac8a64a57265ab8b31f4171b563824f840
Alex Besenev <Besenev96@gmail.com>
2024-02-11 13:23:59 +0500

    Комит в ветке while-for с вводом числа в терминале

--- 4dz/Program.cs
+++ 4dz/Program.cs
@@ -3,40 +3,68 @@
  а на выходе показывает его цифры через запятую.
 Решить с обратным порядком цифр, в эталонном решении такой вариант.
 */
+using System;
+class Program
+{
+static void Main()
+{
+Console.Write("Начнем?1 - да, 0 - нет: ");
+int otvet = Convert.ToInt32(Console.ReadLine());
+while (otvet == 1)
+{
+    Console.Write("Введите число n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
 
-int n = 123456;
-int n_while = n;
-int quantity_num = 1; // количество знаков
-if (n > 9)
-{
-    while (quantity_num < n_while) // узнаем скольки значное число
-    {
-        quantity_num++;
-        n_while /= 10;
-    }
-    // число 10^кол-ва знаков
+    // int n = 9;
+    int n_vichet = n;
+    int n_while = n;
+    int quantity_num = 0; // количество знаков
     int Pow = 1;
-    for (int i = 1; i < quantity_num; i++)
+    if (n > 9)
     {
-        Pow *= 10;
+        while (0 < n_while) // узнаем скольки значное число
+        {
+            quantity_num++;
+            n_while /= 10;
+        }
+            // число 10^кол-ва знаков
+        for (int i = 1; i < quantity_num; i++)
+        {
+            Pow *= 10;
+        }
+        // System.Console.WriteLine("Деление на "+ Pow);
+        while (Pow > 0)
+        {
+            int vichet = n_vichet/Pow;
+            if(n_vichet >= 10)
+            {
+                System.Console.Write(vichet +",");
+            }
+            else 
+            {
+                System.Console.WriteLine(vichet);
+            }
+            n_vichet = n_vichet - (vichet*Pow);
+            Pow /=10;
+        }
+        // for (int i = Pow; i > 0; i /= 10)
+        // {
+        //     int vichet = n/i;
+        //     if(n >= 10)
+        //     {
+        //         System.Console.Write(vichet +",");
+        //     }
+        //     else 
+        //     {
+        //         System.Console.Write(vichet);
+        //     }
+        //     n = n - (vichet*i);
     }
-    // System.Console.WriteLine("Деление на "+ Pow);
-    for (int i = Pow; i > 0; i /= 10)
+    else 
     {
-        int vichet = n/i;
-        if(n >= 10)
-        {
-            System.Console.Write(vichet +",");
-        }
-        else 
-        {
-            System.Console.Write(vichet);
-        }
-        n = n - (vichet*i);
-    
+        System.Console.WriteLine(n);
     }
-}
-else 
-{
-    System.Console.WriteLine(n);
+    Console.Write("Ввести новое число?1 - да, 0 - нет: ");
+    otvet = Convert.ToInt32(Console.ReadLine());}
+    }
 }
