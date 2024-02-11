b5f512ef8b86bf6be9e86de8926e20c52f97e4fa
Alex Besenev <Besenev96@gmail.com>
2024-02-11 14:09:18 +0500

    Добавил комменты по работе кода

--- 4dz/Program.cs
+++ 4dz/Program.cs
@@ -15,13 +15,13 @@ class Program
             Console.Write("Введите любое число n: ");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            int n_vichet = n;
-            int n_while = n;
+            int n_vichet = n; // Для цикла деления на Pow
+            int n_while = n; // Для цикла деления на 10
             int quantity_num = 0; // количество знаков
-            int Pow = 1;
-            if (n > 9)
+            int Pow = 1; // 10 в степени quantity_num
+            if (n > 9)  // отсеем 1 значные числа
             {
-                while (0 < n_while) // узнаем скольки значное число
+                while (0 < n_while) // узнаем скольки значное число делением на 10, до тех пор пока n = 0.
                 {
                     quantity_num++;
                     n_while /= 10;
@@ -32,12 +32,17 @@ class Program
                 {
                     System.Console.WriteLine("Ваше число " + quantity_num + " значное.");
                 }
-                // число 10^кол-ва знаков
+                // Максимальное значение Pow = число 10^ quantity_num (кол-ва знаков)
                 for (int i = 1; i < quantity_num; i++)
                 {
                     Pow *= 10;
                 }
-                // System.Console.WriteLine("Деление на "+ Pow);
+                /* Работа Главного цикл Пример: 
+                1) 1234 / 1000(pow)= CW (1,) ; 
+                2) 1234 - 1*pow = 234;
+                3) 1000/10= 100 - newPow;
+                4) repit
+                */
                 while (Pow > 0)
                 {
                     int vichet = n_vichet / Pow;
