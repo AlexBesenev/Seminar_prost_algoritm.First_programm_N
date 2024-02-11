a5f193d7e274c002ea33e6c03e8a45d9d8c3e153
Alex Besenev <Besenev96@gmail.com>
2024-02-11 13:55:36 +0500

    Версия программы с запросом, с решением проблемы 1000 кратных чисел

--- 4dz/Program.cs
+++ 4dz/Program.cs
@@ -6,65 +6,66 @@
 using System;
 class Program
 {
-static void Main()
-{
-Console.Write("Начнем?1 - да, 0 - нет: ");
-int otvet = Convert.ToInt32(Console.ReadLine());
-while (otvet == 1)
-{
-    Console.Write("Введите число n: ");
-    int n = Convert.ToInt32(Console.ReadLine());
+    static void Main()
+    {
+        Console.Write("Привет, эта программа перечислит все цифры натурального числа через запятую. Если хочешь начать введи 1: ");
+        int otvet = Convert.ToInt32(Console.ReadLine());
+        while (otvet == 1)
+        {
+            Console.Write("Введите любое число n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
 
-    // int n = 9;
-    int n_vichet = n;
-    int n_while = n;
-    int quantity_num = 0; // количество знаков
-    int Pow = 1;
-    if (n > 9)
-    {
-        while (0 < n_while) // узнаем скольки значное число
-        {
-            quantity_num++;
-            n_while /= 10;
-        }
-            // число 10^кол-ва знаков
-        for (int i = 1; i < quantity_num; i++)
-        {
-            Pow *= 10;
-        }
-        // System.Console.WriteLine("Деление на "+ Pow);
-        while (Pow > 0)
-        {
-            int vichet = n_vichet/Pow;
-            if(n_vichet >= 10)
+            int n_vichet = n;
+            int n_while = n;
+            int quantity_num = 0; // количество знаков
+            int Pow = 1;
+            if (n > 9)
             {
-                System.Console.Write(vichet +",");
+                while (0 < n_while) // узнаем скольки значное число
+                {
+                    quantity_num++;
+                    n_while /= 10;
+                }
+                Console.Write("Введи 1 если хотите узнать скольки значное число n : ");
+                int how_quan_num = Convert.ToInt32(Console.ReadLine());
+                if (how_quan_num == 1)
+                {
+                    System.Console.WriteLine("Ваше число " + quantity_num + " значное.");
+                }
+                // число 10^кол-ва знаков
+                for (int i = 1; i < quantity_num; i++)
+                {
+                    Pow *= 10;
+                }
+                // System.Console.WriteLine("Деление на "+ Pow);
+                while (Pow > 0)
+                {
+                    int vichet = n_vichet / Pow;
+                    if (n_vichet >= 10)
+                    {
+                        System.Console.Write(vichet + ",");
+                    }
+                    else
+                    {
+                        if (vichet == 0 && Pow > 1)
+                        {
+                            System.Console.Write(vichet + ",");
+                        }
+                        else 
+                        {
+                            System.Console.WriteLine(vichet);
+                        }
+                    }
+                    n_vichet = n_vichet - (vichet * Pow);
+                    Pow /= 10;
+                }
             }
-            else 
+            else
             {
-                System.Console.WriteLine(vichet);
+                System.Console.WriteLine("Число 1 значное: "+n);
             }
-            n_vichet = n_vichet - (vichet*Pow);
-            Pow /=10;
+            Console.Write("Введи 1, если хочешь повторить: ");
+            otvet = Convert.ToInt32(Console.ReadLine());
         }
-        // for (int i = Pow; i > 0; i /= 10)
-        // {
-        //     int vichet = n/i;
-        //     if(n >= 10)
-        //     {
-        //         System.Console.Write(vichet +",");
-        //     }
-        //     else 
-        //     {
-        //         System.Console.Write(vichet);
-        //     }
-        //     n = n - (vichet*i);
-    }
-    else 
-    {
-        System.Console.WriteLine(n);
-    }
-    Console.Write("Ввести новое число?1 - да, 0 - нет: ");
-    otvet = Convert.ToInt32(Console.ReadLine());}
     }
 }
