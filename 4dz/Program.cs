40cb70870f8328d42b30c1a1629b8a0cfa76c510
Alex Besenev <Besenev96@gmail.com>
2024-02-11 12:41:23 +0500

    Добавили условие для 1 зн. чисел

--- 4dz/Program.cs
+++ 4dz/Program.cs
@@ -4,32 +4,39 @@
 Решить с обратным порядком цифр, в эталонном решении такой вариант.
 */
 
-int n = 2;
+int n = 123456;
 int n_while = n;
 int quantity_num = 1; // количество знаков
-while (quantity_num < n_while) // узнаем скольки значное число
+if (n > 9)
 {
-    quantity_num++;
-    n_while /= 10;
+    while (quantity_num < n_while) // узнаем скольки значное число
+    {
+        quantity_num++;
+        n_while /= 10;
+    }
+    // число 10^кол-ва знаков
+    int Pow = 1;
+    for (int i = 1; i < quantity_num; i++)
+    {
+        Pow *= 10;
+    }
+    // System.Console.WriteLine("Деление на "+ Pow);
+    for (int i = Pow; i > 0; i /= 10)
+    {
+        int vichet = n/i;
+        if(n >= 10)
+        {
+            System.Console.Write(vichet +",");
+        }
+        else 
+        {
+            System.Console.Write(vichet);
+        }
+        n = n - (vichet*i);
+    
+    }
 }
-// число 10^кол-ва знаков
-int Pow = 1;
-for (int i = 1; i < quantity_num; i++)
+else 
 {
-    Pow *= 10;
-}
-// System.Console.WriteLine("Деление на "+ Pow);
-for (int i = Pow; i > 0; i /= 10)
-{
-    int vichet = n/i;
-     if(n >= 10)
-     {
-        System.Console.Write(vichet +",");
-     }
-     else 
-     {
-        System.Console.Write(vichet);
-     }
-    n = n - (vichet*i);
-   
+    System.Console.WriteLine(n);
 }
